9bafd1bdf372f3bb86280bd01cb9ca3190016530
riyazahuja <riyaza@andrew.cmu.edu>
2024-06-06 21:45:33 -0400

    Update lakefile.lean

--- lakefile.lean
+++ lakefile.lean
@@ -8,8 +8,6 @@ package «Tests» where
   ]
   -- add any additional package configuration options here
 
-require mathlib from git
-  "https://github.com/leanprover-community/mathlib4.git"
 
 @[default_target]
 lean_lib «Tests» where
